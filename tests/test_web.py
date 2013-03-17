9e023bd27417722af9839f01a5d08005b2bcdda8
Kai Blin <kai.blin@biotech.uni-tuebingen.de>
2013-03-17 13:09:12 +0100

    tests: Fix download page test
    Signed-off-by: Kai Blin <kai.blin@biotech.uni-tuebingen.de>

--- tests/test_web.py
+++ tests/test_web.py
@@ -16,7 +16,7 @@ class WebTestCase(WebsmashTestCase):
     def test_downloadpage(self):
         """Test if download page works"""
         rv = self.client.get('/download')
-        assert "The current version of antiSMASH is 1.2" in rv.data
+        assert "The current standalone release is antiSMASH <strong>2.0.0</strong> (<em>February 24th, 2013</em>)" in rv.data
 
     def test_helppage(self):
         """Test if help page works"""
@@ -210,7 +210,7 @@ class WebTestCase(WebsmashTestCase):
     def test_compat_downloadpage(self):
         """Test if old download page link works"""
         rv = self.client.get('/download.html')
-        assert "The current version of antiSMASH is 1.2" in rv.data
+        assert "The current standalone release is antiSMASH <strong>2.0.0</strong> (<em>February 24th, 2013</em>)" in rv.data
 
     def test_compat_helppage(self):
         """Test if old help page link works"""
