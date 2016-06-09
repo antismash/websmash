7e07d749ec666beee5fa8e7fdad007e17726453e
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-09 13:58:03 +0200

    Add support for Redis Sentinel connections
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- websmash/__init__.py
+++ websmash/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask, g
 from flask.ext.mail import Mail
 from werkzeug import SharedDataMiddleware
 from os import path
+from urlparse import urlparse
 from redis import Redis
+from redis.sentinel import Sentinel
 
 app = Flask(__name__)
 import websmash.default_settings
@@ -22,7 +24,19 @@ def get_db():
             from mockredis import mock_redis_client
             redis_store = g._database = mock_redis_client()
         else:
-            redis_store = g._database = Redis.from_url(app.config['REDIS_URL'])
+            if app.config['REDIS_URL'].startswith('redis://'):
+                redis_store = g._database = Redis.from_url(app.config['REDIS_URL'])
+            elif app.config['REDIS_URL'].startswith('sentinel://'):
+                parsed_url = urlparse(app.config['REDIS_URL'])
+                service = parsed_url.path.lstrip('/')
+                port = 26379
+                if ':' in parsed_url.netloc:
+                    host, str_port = parsed_url.netloc.split(':')
+                    port = int(str_port)
+                else:
+                    host = parsed_url.netloc
+                sentinel = Sentinel([(host, port)], socket_timeout=0.1)
+                redis_store = sentinel.master_for(service, redis_class=Redis, socket_timeout=0.1)
     return redis_store
 
 import websmash.models
