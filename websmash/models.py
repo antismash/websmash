402267f784bbd1c7df0993f752a58682653039aa
Kai Blin <kblin@biosustain.dtu.dk>
2017-12-12 18:16:56 +0100

    models: remove unused function get_bool
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- websmash/models.py
+++ websmash/models.py
@@ -1,14 +1,6 @@
 import uuid
 
 
-def get_bool(obj, param, default=False):
-    # type: (dict, str, bool) -> bool
-    """convert Redis' string serialised boolean values back to true booleans"""
-    val = obj.get(param, default)
-    if isinstance(val, str):
-        val = (val.lower() == 'true')
-    return val
-
 def _generate_jobid(taxon):
     """Generate a job uid based on the taxon"""
     return "{}-{}".format(taxon, uuid.uuid4())
